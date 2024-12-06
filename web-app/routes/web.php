4e3cc6c4e72c5ab13bee6f06fadbfdf37eebd397
wawerks <rhowinlumales@gmail.com>
2024-12-06 23:52:58 +0800

    nara gaw

--- web-app/routes/web.php
+++ web-app/routes/web.php
@@ -4,14 +4,21 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\SocialiteController;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
+    return Inertia::render('LandingPage', [
         'canLogin' => Route::has('login'),
         'canRegister' => Route::has('register'),
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
+});
+
+// Socialite Google Authentication Routes
+Route::controller(SocialiteController::class)->group(function () {
+    Route::get('auth/google', 'googleLogin')->name('auth.google');
+    Route::get('auth/google-callback', 'googleAuthentication')->name('auth.google-callback');
 });
 
 Route::get('/dashboard', function () {
