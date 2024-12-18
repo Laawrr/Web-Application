a55644be8a2a6c6b80f26b8eec1fd2b37d3cd047
wawerks <rhowinlumales@gmail.com>
2024-12-18 12:39:17 +0800

    navbarsaodiqjw

--- web-app/resources/js/Pages/Auth/Login.vue
+++ web-app/resources/js/Pages/Auth/Login.vue
@@ -31,17 +31,18 @@
                 </label>
                 <Link v-if="canResetPassword" :href="route('password.request')"
                     class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Forgot your password?
+                Forgot your password?
                 </Link>
             </div>
 
             <div class="mt-6">
-                <PrimaryButton class="w-full justify-center" :class="{ 'opacity-50 cursor-not-allowed': form.processing }"
-                    :disabled="form.processing">
+                <PrimaryButton class="w-full justify-center"
+                    :class="{ 'opacity-50 cursor-not-allowed': form.processing }" :disabled="form.processing">
                     <template v-if="form.processing">
-                        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
-                            viewBox="0 0 24 24">
-                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                        <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg"
+                            fill="none" viewBox="0 0 24 24">
+                            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4">
+                            </circle>
                             <path class="opacity-75" fill="currentColor"
                                 d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
                             </path>
@@ -56,7 +57,7 @@
                 <span class="text-sm text-gray-600">Don't have an account? </span>
                 <Link :href="route('register')"
                     class="text-sm text-teal-600 hover:text-teal-500 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                    Register now
+                Register now
                 </Link>
             </div>
         </form>
